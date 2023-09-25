9385ef4f64ee27b09c8d90e32774ba2035071885
Michael Lysik <michael.lysik@mymail.champlain.edu>
2023-09-24 21:16:57 -0400

    RNG

--- courses/artificialintelligence/assignments/life/life.cpp
+++ courses/artificialintelligence/assignments/life/life.cpp
@@ -1,3 +1,21 @@
+#include <iostream>
 int main(){
+    // generate grid
+    int x_length = 0;
+    int y_length = 0;
+    int numTurns = 0;
+    //nu
+    std::cin >> x_length;
+    std::cin >> y_length;
+    std::cin >> numTurns;
+    for(int y = 0; y < y_length; y++)
+    {
+        for(int x = 0; x < x_length; x++ )
+        {
+            std::cout << ".";
+        }
+        std::cout << "\n";
+    }
+
   return 0;
 };
--- courses/artificialintelligence/assignments/rng/rng.cpp
+++ courses/artificialintelligence/assignments/rng/rng.cpp
@@ -1,4 +1,29 @@
 // add your imports here
+#include <iostream>
 int main(){
   // code here
+  unsigned int seed = 0;     //S
+  unsigned int randNums = 0; //N
+  signed int min = 0;         //R1 (min)
+  signed int max = 0;         //R2 (max)
+  int value = 0;
+
+  std::cin >> seed >> randNums >> min >> max;
+  //std::cout << "seed: " << seed << " randNums: " << randNums << " R1: " << R1 << " R2: " << R2 << "\n";
+  //value = seed;
+
+  for(int i = 0; i < randNums; i++)
+  {
+      seed = seed xor (seed << 13);
+
+      seed = seed xor (seed >> 17);
+
+      seed = seed xor (seed << 5);
+
+      value = min + (seed % (max - min + 1));
+
+      std::cout << value << "\n";
+  }
+
+
 }
